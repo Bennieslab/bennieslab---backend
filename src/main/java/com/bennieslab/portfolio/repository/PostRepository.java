83eacf305c67bd2cb7a601754c6fbefbb21ba64d
Bensonmusonda <bensonmusonda12@gmail.com>
2026-07-12 20:16:59 +0000

    fix: 500 server error on post and project controller

--- src/main/java/com/bennieslab/portfolio/repository/PostRepository.java
+++ src/main/java/com/bennieslab/portfolio/repository/PostRepository.java
@@ -1,5 +1,9 @@
 package com.bennieslab.portfolio.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,12 @@ import com.bennieslab.portfolio.model.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
+    @Override
+    @EntityGraph(attributePaths = "skills")
+    List<Post> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = "skills")
+    Optional<Post> findById(Long id);
 
 }
--- src/main/java/com/bennieslab/portfolio/repository/ProjectRepository.java
+++ src/main/java/com/bennieslab/portfolio/repository/ProjectRepository.java
@@ -1,7 +1,9 @@
 package com.bennieslab.portfolio.repository;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +13,14 @@ import com.bennieslab.portfolio.repository.mini.ProjectMini;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    @Query("SELECT p FROM Project p")
+    @Override
+    @EntityGraph(attributePaths = "skills")
+    List<Project> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = "skills")
+    Optional<Project> findById(Long id);
+
+    @Query("SELECT p.id AS id, p.name AS name FROM Project p")
     List<ProjectMini> findAllProjectMini();
 }
